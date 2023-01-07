a677e503a59766de3ac1f66fd3c8ec790f4ead58
Daniel Tremura <tremuradaniel@gmail.com>
2023-01-07 16:27:18 +0200

    configuring spring mvc controllers

--- readme.md
+++ readme.md
@@ -5,3 +5,6 @@ Check the app log when it starts for the JDBC URL
 
 <h3>Spring MVC</h3>
 ![spring mvc](src/main/resources/static/readme/spring-mvc.png)
+
+<h3>Spring Controllers</h3>
+![spring mvc](src/main/resources/static/readme/spring%20controller.png)
---
+++ src/main/java/simpleSpringWebApp/springwebapp/controllers/BookController.java
@@ -0,0 +1,23 @@
+package simpleSpringWebApp.springwebapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import simpleSpringWebApp.springwebapp.repositories.BookRepository;
+
+@Controller
+public class BookController {
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", this.bookRepository.findAll());
+        return "books";
+    }
+
+
+}
